a98a82ad93c630f0d1742522f6a28e9dc83e8883
1064517852@qq.com <w1994z0115x>
2020-12-22 17:14:52 +0800

    简单使用redisTemplate

---
+++ noxus-draven-springboot-2.4.x/noxus-draven-2.4.x-caching/noxus-draven-2.4.x-caching-redis/src/main/java/com/noxus/draven/caching/rediss/config/RedisConfig.java
@@ -0,0 +1,50 @@
+package com.noxus.draven.caching.rediss.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * 〈功能概述〉<br>
+ *
+ * @className: RedisConfig
+ * @package: com.noxus.draven.caching.rediss.config
+ * @author: draven
+ * @date: 2020/12/22 16:39
+ */
+@Configuration
+public class RedisConfig{
+
+    /**
+     * 申明缓存管理器，会创建一个切面（aspect）并触发Spring缓存注解的切点（pointcut）
+     * 根据类或者方法所使用的注解以及缓存的状态，这个切面会从缓存中获取数据，将数据添加到缓存之中或者从缓存中移除某个值
+
+     * @return
+     */
+    @Bean
+    public RedisTemplate redisTemplate(RedisConnectionFactory factory) {
+        // 创建一个模板类
+        RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        // 将刚才的redis连接工厂设置到模板类中
+        template.setConnectionFactory(factory);
+        // 设置key的序列化器
+        template.setKeySerializer(new StringRedisSerializer());
+        // 设置value的序列化器
+        //使用Jackson 2，将对象序列化为JSON
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        //json转对象类，不设置默认的会将json转成hashmap
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+
+        return template;
+    }
+}
